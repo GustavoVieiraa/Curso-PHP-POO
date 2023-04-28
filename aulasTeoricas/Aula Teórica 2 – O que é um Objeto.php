d4be44eb8cceefab7975c1e6f405b98b1d9ecd43
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-04-28 12:42:09 -0300

    Aula Teórica 2 – O que é um Objeto - concluída com sucesso!

---
+++ aulasTeoricas/Aula Teórica 2 – O que é um Objeto.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 2 – O que é um Objeto?</title>
+</head>
+<body>
+    <section>
+        <h1>O que é um Objeto?</h1>
+        <p>Um objeto é uma coisa material ou abstrata que pode ser percebida pelos sentidos e descrita por meio das suas características, comportamentos e estado atual.</p>
+        <h1>Caracteristicas de um Objeto:</h1>
+        <ul> <p>Todo Objeto tem:</p>
+            <li>Atributos</li>
+            <li>Métodos</li>
+            <li>Estado</li>
+        </ul>
+        <p>Primeiro, antes de termos o nosso objeto, temos que criar a sua classe e atribuir a ela os atributos, metodos e estado que, quando ao <strong>instanciar</strong> os nossos objetos eles venham herdar esses atributos, metodos e estados.</p>
+        <h1>Instanciar</h1>
+        <p>Nós utilizamos as classes para <strong>instanciar</strong> objetos. Não transformamos uma classe em um objeto, geramos um objeto a partir de uma classe.</p>
+        <h1>Classe</h1>
+        <p>Uma classe define os atributos e métodos comuns que serão compartilhados por outros objetos.</p>
+        <h1>Objeto</h1>
+        <p>É uma instância de uma classe.</p>
+    </section>
+</body>
+</html>
