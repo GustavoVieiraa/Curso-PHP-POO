bd912fb5f1f6ebb51f5cf44b2e985ec7a2b5e3c5
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-04-28 15:19:44 -0300

    correção

--- aulasPraticas/Aula Prática 2 – Criando Classes e Objetos em PHP.php
+++ aulasPraticas/Aula Prática 2 – Criando Classes e Objetos em PHP.php
@@ -4,9 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
     <title>Criando Classes e Objetos em PHP</title>
 </head>
 <body>
+    <section>
+        <?php 
+            require_once '../classes/Caneta.php';
+            $caneta1 = new Caneta();
+            $caneta1->cor = "Rosa";
+            $caneta1->ponta = 0.7;
+            $caneta1->carga = 70;
+            $caneta1->modelo = 'BIC';
+            $caneta1->tampada = false;
+            $caneta1->tampar(); echo "<br>";
+            $caneta1->rabiscar(); echo "<br>";
+            $caneta1->eNova();
+            print_r($caneta1);
 
+            $caneta2 = new Caneta();
+            $caneta2->cor = "Azul";
+            $caneta2->ponta = 0.7;
+            $caneta2->carga = 100;
+            $caneta2->modelo = 'BIC';
+            $caneta2->tampada = true;
+            $caneta2->rabiscar(); echo "<br>";
+            $caneta2->eNova(); echo "<br>";
+            print_r($caneta2)
+        ?>
+    </section>
 </body>
 </html>
--- classes/Caneta.php
+++ classes/Caneta.php
@@ -1,21 +1,34 @@
 <?php
- 
+
+
+
 class Caneta {
     var $modelo;
     var $cor;
-    var $Ponta;
+    var $ponta;
     var $carga;
     var $tampada;
 
     function rabiscar() {
-
-    }
-
-    function tampar() {
+        if ($this->tampada == true) {
+            echo "<p>ERRO! Não é possível rabiscar! Sua caneta está tampada.";
+        } else {
+            echo "<p>Estou rabiscando o HTML5 &copy;</p>";
+        }
         
     }
 
-    function destampar() {
+    function eNova() {
+        if ($this->carga == 100) {
+            echo "Caneta nova!";
+        }
+    }
 
+    function tampar() {
+        $this->tampada = true;
+    }
+
+    function destampar() {
+        $this->tampada = false;
     }
 }
