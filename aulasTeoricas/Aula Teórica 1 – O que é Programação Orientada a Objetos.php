de40e9ce41d8f2a4f1f9a8856e1175003aa351e0
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-04-28 12:38:27 -0300

    O que é Programação Orientada a Objetos - aula concluída com êxito!

---
+++ aulasTeoricas/Aula Teórica 1 – O que é Programação Orientada a Objetos.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 1 – O que é Programação Orientada a Objetos?</title>
+</head>
+<body>
+    <section>
+        <h1>Aula Teórica 1 – O que é Programação Orientada a Objetos?</h1>
+        <p>O que significa POO?</p>
+        <p>Programação Orientada a Objeto</p>
+        <p>Em inglês: OOP (Object Oriented Programming)</p>
+        <h1>Como era a programação antes da POO?</h1>
+        <p>Programação Baixo Nivel > Programação Linear -> Programação Estruturada -> Programação Modular -> <strong>POO</strong></p>
+        <h1>Quem criou a primeira Linguagem POO?</h1>
+        <p>Foi Alan Kay, formado em matemática e biologia, ele é o Pai da Programação orientada a objeto.</p>
+        <h1>Qual o objetivo da programação Orientada a Objeto?</h1>
+        <p>É simples, aproximar o mundo digital do mundo real!</p>
+        <h1>Linguagens POO</h1>
+        <ul>
+            <li>C++</li>
+            <li>JAVA</li>
+            <li>PHP</li>
+        </ul>
+        <h1>Vantagens de Programar Orientado a Objeto</h1>
+        <ul>
+            <li>Confiável - O isolamento entre as partes gera software seguro. Ao alterar uma parte, nenhuma outra é afetada.</li>
+            <li>Oportuno - Ao dividir tudo em partes, várias delas podem ser desenvolvidas em paralelo.</li>
+            <li>Manutenível - Atualizar um software é mais fácil. Uma pequena modificação vai beneficiar todas as partes que usarem o objeto.</li>
+            <li>Extensível - O software não é estático. Ele deve crescer para permanecer útil.</li>
+            <li>Reutilizável - Podemos usar objetos de um sistema que criamos em outro sistema no futuro.</li>
+            <li>Natural - Mais fácil de entender. Você se preocupa mais na funcionalidade do que nos detalhes de implementação.</li>
+        </ul>
+    </section>
+</body>
+</html>
