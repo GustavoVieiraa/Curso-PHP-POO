2dd06ec2d3859372b92cb415f2bbecaf4bb8bb25
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-04-28 16:10:22 -0300

    Aula Teórica 3 - O que é Visibilidade em um Objeto - concluída com êxito!

---
+++ aulasTeoricas/Aula Teórica 3 – O que é Visibilidade em um Objeto.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Visibilidade em um Objeto</title>
+</head>
+<body>
+    <section>
+        <h1>O que é Visibilidade em um Objeto?</h1>
+        <p>Modificadores de Visibilidade - Indicam o nível de acesso aos componentes internos de uma classe.</p>
+        <ul>
+            <li>+ PUBLICA - A classe atual e todas as outras classes.</li>
+            <li>- PRIVADO - Somente a classe atual pode alterar.</li>
+            <li># PROTEGIDO - A classe atual e todas as suas sub-classes.</li>
+        </ul>
+        <p>Primeiro você já ouviu falar em Linguagem de Modelagem Unificada?</p>
+        <h1>Linguagem de Modelagem Unificada (UML)</h1>
+        <p>Vamos conhecer algumas técnicas, como o Diagrama de Classes.</p>
+        <p>Diagrama de Classes - No diagrama de classes toda classe é um retangulo.</p>
+        <img style="height: 400px; border-radius: 10px" src="https://www.guj.com.br/uploads/default/original/3X/7/c/7c6b9aee1e95a7f427c2367317a4ee44038e2ca1.jpg_" alt="exemplo-img">
+    </section>
+</body>
+</html>
