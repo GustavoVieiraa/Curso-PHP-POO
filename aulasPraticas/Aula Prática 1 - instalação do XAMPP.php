2a2d3edb7a41d1750d549755ad9e771f8362a9a2
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-04-28 12:45:13 -0300

    Aula Prática 1 - instalação do XAMPP - prática concluída com êxito!

---
+++ aulasPraticas/Aula Prática 1 - instalação do XAMPP.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Instalando Softwares</title>
+</head>
+<body>
+    <section>
+        <h1>Instalando XAMPP</h1>
+        <p>Nessa aula prática, fizemos todas as instalações necessárias para rodar o nosso servidor e nosso PHP.</p>
+    </section>
+</body>
+</html>
