d318f961b3a94dfc82014e803a7a20dc27d75cec
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-05-17 23:15:00 -0300

    Aula Teórica 4 - Métodos Especiais - concluída com êxito!

---
+++ aulasTeoricas/Aula Teórica 4 – Métodos Especiais.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 4 – Métodos Especiais</title>
+</head>
+<body>
+    <header>
+        <h1>Aula Teórica 4 – Métodos Especiais</h1>
+    </header>
+    <section>
+        <h1>Métodos Acessores/Getters</h1>
+        <p>Métodos acessores, ou Getters são métodos que garantem o acesso a um determinado atributo garantindo a segurança deles.</p>
+        <h1>Métodos Modificadores/Setters</h1>
+        <p>Métodos Modificadores são literais, servem para modificar algo em um objeto instanciado através de Setters/Set passando a modificação através de um parametro.</p>
+        <h1>Método Construtor/Construct</h1>
+        <p>É o método que utilizamos para construir um padrão de uma classe para novas instancias de objetos.</p>
+    </section>
+</body>
+</html>
