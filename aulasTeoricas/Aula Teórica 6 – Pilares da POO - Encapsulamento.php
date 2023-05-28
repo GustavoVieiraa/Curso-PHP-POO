f0e7d8c4058caf69d1c2db7a58bdb7c26106842b
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-05-28 00:28:12 -0300

    Aula Téorica 6 - Encapsulamento - finalizada com êxito!

---
+++ aulasTeoricas/Aula Teórica 6 – Pilares da POO - Encapsulamento.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 6 – Pilares da POO: Encapsulamento</title>
+</head>
+<body>
+    <section>
+        <h1>Pilares da POO: Encapsulamento</h1>
+        <h2>
+            <ul><p>Os três pilares da POO:</p>
+                <li>Encapsulamento</li>
+                <li>Herança</li>
+                <li>Polimorfismo</li>
+            </ul>
+        </h2>
+        <p>Alerta! Encapsular não é obrigatório, mas é uma boa prática para produzir Classes mais eficientes.</p>
+        <h1>Vantagens em Encapsular: </h1>
+        <ul>
+            <li><p>1. Tornar mudanças invisiveis.</p></li>
+        </ul>
+        <ul>
+            <li><p>2. Facilitar reutilização do código.</p></li>
+        </ul>
+        <ul>
+            <li><p>3. Reduzir os efeitos colaterais.</p></li>
+        </ul>
+        <h1>Como encapsular?</h1>
+        <p>Vamos encapsular na Aula Prática - Encapsulamento.</p>
+    </section>
+</body>
+</html>
