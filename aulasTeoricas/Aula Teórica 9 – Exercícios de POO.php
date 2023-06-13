c5cc248194a4ce568ad5744c3e071bf850593ea9
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-06-13 16:36:43 -0300

    Aula Teórica 9 - Exercícios de POO - concluída com êxito!

---
+++ aulasTeoricas/Aula Teórica 9 – Exercícios de POO.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 9 – Exercícios de POO</title>
+</head>
+<body>
+    <section>
+        <h1>Aula executada com êxito!</h1>
+    </section>
+</body>
+</html>
