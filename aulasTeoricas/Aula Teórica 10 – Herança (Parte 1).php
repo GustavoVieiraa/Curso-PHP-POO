55b16bcb8d5afaa962300c55311f9c8fdc458ea2
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-06-16 12:21:47 -0300

    Aula teórica 10 - Herança (Parte 1) - concluída com êxito!

---
+++ aulasTeoricas/Aula Teórica 10 – Herança (Parte 1).php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 10 – Herança (Parte 1)</title>
+</head>
+<body>
+    <section>
+        <h1>Herança</h1>
+        <p>Permite basear uma nova classe na definição de uma outra classe previamente existente.</p>
+        <p>A herança será aplicada tanto para as características quanto para os comportamentos (Atributos/Metodos)  </p>
+    </section>
+</body>
+</html>
