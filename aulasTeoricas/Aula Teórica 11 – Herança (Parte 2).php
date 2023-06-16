bac4f43da2bd4a97f166743edb2273e104eab4d8
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-06-16 16:53:29 -0300

    Aula Teórica 11 - Herança (Parte 2) - concluída com êxito!

---
+++ aulasTeoricas/Aula Teórica 11 – Herança (Parte 2).php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 11 – Herança (Parte 2)</title>
+</head>
+<body>
+    <section>
+        <h1>Navegação pela Herança - Aprendido!</h1>
+        <h1>Tipos de Herança</h1>
+        <p>Herança de Implementação: É uma herança que ira implementar em suas classes.</p>
+        <p>Herança para Diferença: É uma herança para diferença</p>
+        <strong>Classe abstrata:</strong><p>Não pode ser instanciada. Só pode servir como progenitora.</p>
+        <strong>Classe Método abstrato</strong><p>Declarado, mas não implementado na progenitora</p>
+        <strong>Classe Final</strong><p>Não pode ser herdada por outra classe. Obrigatoriamente folha.</p>
+        <strong>Método Final</strong><p>Não pode ser sobrescrito pelas suas sub-classes. Obrigatoriamente herdado.</p>
+    </section>
+</body>
+</html>
