9fc0cdb72e9a5c1a2ee25e105db65cc41df5b5d4
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-06-19 12:18:17 -0300

    Aula Teórica 12 - Conceito Polimorfismo (Parte 1) - concluída com êxito!

---
+++ aulasTeoricas/Aula Teórica 12 – Conceito Polimorfismo (Parte 1).php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 12 – Conceito Polimorfismo (Parte 1)</title>
+</head>
+<body>
+    <section>
+        <h1>Polimorfismo</h1>
+        <p>Permite que um mesmo nome represente vários comportamento diferentes</p>
+        <p>Em uma metodo onde aplicamos Polimorfismo, temos que nós atentar a quantidade e os tipos dos parâmetros.</p>
+        <h1>Tipos de Polimorfismo</h1>
+        <p>Temos Sobreposição e Sobrecarga</p>
+        <p>Polimorfismo de Sobreposição: Acontece quando substituímos um método de uma superclasse na sua subclasse, usando a mesma assinatura.</p>
+    </section>
+</body>
+</html>
