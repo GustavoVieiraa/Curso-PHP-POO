f0a203cc7c94737e0b679a89806edf44dacac20a
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-06-20 12:32:35 -0300

    Aula Prática 13 - Polimorfismo Sobrecarga (Parte 2) - Concluída com êxito!

---
+++ aulasPraticas/Aula Prática 13 – Polimorfismo Sobrecarga (Parte 2).php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Prática 13 – Polimorfismo Sobrecarga (Parte 2)</title>
+</head>
+<body>
+    <section>
+        <h1>Sobrecarga</h1>
+        <p>Sobrecarga: Utilizamos ela com a assinatura diferente, mas na mesma classe.</p>
+        <h1>Não existe SOBRECARGA em PHP.</h1>
+    </section>
+</body>
+</html>
