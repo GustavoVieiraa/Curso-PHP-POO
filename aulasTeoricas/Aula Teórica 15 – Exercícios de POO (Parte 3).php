978c9dece98bc3e489b6254982edddddc0b342e8
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-06-20 13:51:54 -0300

    Aula Teórica 15 – Exercícios de POO (Parte 3) - concluída com êxito!

---
+++ aulasTeoricas/Aula Teórica 15 – Exercícios de POO (Parte 3).php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilo/style.css">
+    <title>Aula Teórica 15 – Exercícios de POO (Parte 3)</title>
+</head>
+<body>
+    <section>
+        <h1>Exercicios Feitos em Aula com o Vídeo!</h1>
+    </section>
+</body>
+</html>
